169ddc65b54d00944cb1ea5d481b278624241dfd
karzia <karzia01@gmail.com>
2024-06-01 19:29:55 +0900

    removed useless functions

--- lib/bubble_chat.dart
+++ lib/bubble_chat.dart
@@ -3,30 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-enum ChatUser{
-  me("user"),
-  agent("assistant")
-  ;
-
-  final String name;
-  const ChatUser(this.name);
-}
-extension ChatUserExt on String{
-  get toUser {
-    if( this== "user" || this== "me") return ChatUser.me;
-    return ChatUser.agent;
-  }
-}
-
-W(double v) => 1*v;
-extension Iext on int{
-  double get wr => toDouble();
-  double get hr => toDouble();
-}
 enum ChatPos{
   left,
   right,
 }
+
 
 
 Widget renderBubble({required ChatPos pos, Widget? name, Widget? content, DateTime? date,}) {
@@ -44,17 +25,17 @@ Widget renderBubble({required ChatPos pos, Widget? name, Widget? content, DateTi
           ),
         if(name!=null)name,
         Container(
-          constraints: BoxConstraints(maxWidth: W(200)),
-          padding: EdgeInsets.all(W(10)),
-          margin: EdgeInsets.only(top: W(5), bottom: 5.wr),
+          constraints: BoxConstraints(maxWidth: 200),
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.only(top: 5, bottom: 5),
           decoration: BoxDecoration(
               color:
               (pos == ChatPos.right) ? Colors.amberAccent.withOpacity(0.5) : Colors.lightBlueAccent.withOpacity(0.5),
               borderRadius: BorderRadius.only(
                 topLeft: (pos == ChatPos.left)  ? Radius.zero : const Radius.circular(20),
                 topRight: (pos == ChatPos.right)  ? Radius.zero : const Radius.circular(20),
-                bottomLeft: Radius.circular(20.wr),
-                bottomRight: Radius.circular(20.wr),
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
               )),
           child: Column(
             mainAxisSize: MainAxisSize.min,
